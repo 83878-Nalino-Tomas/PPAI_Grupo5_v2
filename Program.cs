79b904ffc96559644a565e6427a86daa0c272a0a
Tomas Nalino <tomasnalino@gmail.com>
2022-11-12 12:32:21 -0300

    Ejemplos de uso de EntityFramework

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PPAI_Grupo5_v2.Models;
+using System;
 
 namespace PPAI_Grupo5_v2
 {
@@ -6,7 +7,40 @@ namespace PPAI_Grupo5_v2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var context = new DSI2022v2Context())
+            {
+                //    var user = new Usuario()
+                //    {
+                //        Habilitado = true,
+                //        Clave = "admin123",
+                //        Usuario1 = "ramonroldan",
+                //    };
+                //    context.Usuarios.Add(user);
+                //    context.SaveChanges();
+
+                var cientifico1 = new PersonalCientifico()
+                {
+                    Nombre = "Ramon",
+                    Apellido = "Roldan",
+                    NumeroDocumento = 23392569,
+                    Correo = "ramonroldan@gmail.com",
+                    TelefonoCelular = "23392569",
+                    Legajo = 83877,
+                    Usuario = 2
+                };
+                context.PersonalCientificos.Add(cientifico1);
+                context.SaveChanges();
+
+                //var sesionActual = new Sesion()
+                //{
+                //    FechaHoraInicio = DateTime.Now,
+                //    FechaHoraFin = DateTime.Now.AddHours(2),
+                //    Usuario = user.IdUsuario
+                //};
+                //context.Sesions.Add(sesionActual);
+                //context.SaveChanges();
+
+            }
         }
     }
 }
